77a52feaae89a434f19605ff519b3504e62290ff
ayush3298 <abairagi311@gmail.com>
2023-11-02 19:25:08 +0530

    add multiprocessing to execute code in parallel

--- APIcomparatorlib.py
+++ APIcomparatorlib.py
@@ -1,6 +1,11 @@
 from urllib.request import urlopen, Request
+from concurrent.futures import ThreadPoolExecutor
+
 
 class APIComparator:
+    """
+    APIComparator : Allows you to compare two sets of API responses (HTTP/HTTPS) provided in two input files. 
+    """
     def __init__(self, file1, file2):
         self.file1 = file1
         self.file2 = file2
@@ -29,12 +34,13 @@ class APIComparator:
             print(f"Error comparing {url1} and {url2}: {str(e)}")
             
 
-    def compare_responses(self):
+    def compare_responses(self, num_threads=1):
         with open(self.file1, 'r') as f1, open(self.file2, 'r') as f2:
-            for line1, line2 in zip(f1, f2):
-                url1 = line1.strip()
-                url2 = line2.strip()
-                self.compare_and_print(url1, url2)
+            with ThreadPoolExecutor(max_workers=num_threads) as executor:
+                for line1, line2 in zip(f1, f2):
+                    url1 = line1.strip()
+                    url2 = line2.strip()
+                    executor.submit(self.compare_and_print, url1, url2)
 
     def are_responses_equal(self):
         return not response1 != response2
